6d6c78cd7ab069fab6593c0e6f8350ac1e3b3c07
deAndi <andreas.schmieder@googlemail.com>
2018-02-01 21:10:00 +0100

    Daten werden geholt und können in einer Liste angezeigt werden

--- App.js
+++ App.js
@@ -6,15 +6,16 @@
 
 import React, { Component } from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
+    Platform,
+    StyleSheet,
+    Text,
+    View,
     ToolbarAndroid,
     DrawerLayoutAndroid,
-    TouchableHighlight
+    TouchableHighlight, FlatList, ScrollView
 } from 'react-native';
 import { Header } from 'react-native-elements';
+import Button from "react-native-elements/src/buttons/Button";
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -23,19 +24,62 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-
+let gamesList: any;
 
 export default class App extends Component<{}> {
 
     constructor(){
         super();
-
         this.openDrawer = this.openDrawer.bind(this);
+        this.httpRequest = this.httpRequest.bind(this);
+
+        this.state = {
+            games: 'Moin',
+        }
+
     }
 
     openDrawer() {
         console.log('Geht was');
         this.drawer.openDrawer();
+    }
+
+    httpRequest(){
+        console.log('Hole Daten');
+        let url = "https://www.openligadb.de/api/getmatchdata/bl1/2017";
+        let http = new XMLHttpRequest();
+
+        http.onreadystatechange = function () {
+            if(http.readyState === 4){
+                if(http.status !== 200){
+                    console.log(http.response);
+                    return;
+                }
+                console.log('Verarbeite Daten');
+                this.setState({
+                    games: JSON.parse(http.response)
+                });
+
+                this.gamesList = this.state.games.map((game) => {
+                    if (game.Location !== null) {
+                        return <Text key={game.MatchID}>{game.Location.LocationCity}</Text>
+                    }
+                });
+
+                console.log(this.state.games);
+                console.log(this.state.games.length + " Daten erhalten");
+                for(let game of this.state.games){
+                    if(game.Location !== null){
+                        if(game.Location.LocationCity === 'Freiburg'){
+                            console.log(game);
+                        }
+                    }
+                }
+            }
+        }.bind(this);
+
+        http.open("GET", url, true);
+        http.send();
     }
 
   render() {
@@ -62,6 +106,15 @@ export default class App extends Component<{}> {
                   <Text style={styles.welcome}>
                       Hier kommt die View hin
                   </Text>
+                  <TouchableHighlight onPress={() => this.httpRequest()} underlayColor="white">
+                      <View style={styles.button}>
+                          <Text style={styles.buttonText}>TouchableHighlight</Text>
+                      </View>
+                  </TouchableHighlight>
+                 <ScrollView>
+                     {this.gamesList}
+                 </ScrollView>
+
               </View>
           </DrawerLayoutAndroid>
       );
@@ -86,4 +139,14 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+    button: {
+        marginBottom: 30,
+        width: '100%',
+        alignItems: 'center',
+        backgroundColor: '#2196F3'
+    },
+    buttonText: {
+        padding: 20,
+        color: 'white'
+    }
 });
